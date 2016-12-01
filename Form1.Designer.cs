e80a7fdc2a9b9dfa96cded8a10dcf7d690163880
Michał <michal.dwojak92@gmail.com>
2016-12-01 23:06:08 +0100

    Add - design

---
+++ Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Login_Application
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.label1 = new System.Windows.Forms.Label();
+            this.mtb1 = new System.Windows.Forms.MaskedTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.gb1 = new System.Windows.Forms.GroupBox();
+            this.b1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.gb1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.Crimson;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 165);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(413, 30);
+            this.flowLayoutPanel1.TabIndex = 11;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Crimson;
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(411, 24);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Login:";
+            // 
+            // mtb1
+            // 
+            this.mtb1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.mtb1.Location = new System.Drawing.Point(60, 47);
+            this.mtb1.Name = "mtb1";
+            this.mtb1.PasswordChar = '*';
+            this.mtb1.Size = new System.Drawing.Size(178, 26);
+            this.mtb1.TabIndex = 4;
+            this.mtb1.Text = "hmagazyn1x";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(6, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Hasło:";
+            // 
+            // tb1
+            // 
+            this.tb1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.tb1.Location = new System.Drawing.Point(60, 15);
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(178, 26);
+            this.tb1.TabIndex = 3;
+            this.tb1.Text = "pmagazyn1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.richTextBox1);
+            this.groupBox1.Location = new System.Drawing.Point(255, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(150, 73);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Testowe dane";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.richTextBox1.Location = new System.Drawing.Point(6, 16);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(138, 46);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "Login: pmagazyn1\nHasło: hmagazyn1x";
+            // 
+            // gb1
+            // 
+            this.gb1.Controls.Add(this.label1);
+            this.gb1.Controls.Add(this.mtb1);
+            this.gb1.Controls.Add(this.label2);
+            this.gb1.Controls.Add(this.tb1);
+            this.gb1.Location = new System.Drawing.Point(5, 27);
+            this.gb1.Name = "gb1";
+            this.gb1.Size = new System.Drawing.Size(244, 86);
+            this.gb1.TabIndex = 12;
+            this.gb1.TabStop = false;
+            // 
+            // b1
+            // 
+            this.b1.BackColor = System.Drawing.Color.White;
+            this.b1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.b1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.b1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.b1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b1.Location = new System.Drawing.Point(142, 117);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(111, 40);
+            this.b1.TabIndex = 9;
+            this.b1.Text = "Zaloguj";
+            this.b1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(411, 192);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.gb1);
+            this.Controls.Add(this.b1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.gb1.ResumeLayout(false);
+            this.gb1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox mtb1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.GroupBox gb1;
+        private System.Windows.Forms.Button b1;
+    }
+}
+
