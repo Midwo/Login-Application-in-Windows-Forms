f5510cb7daec7778a42807d2474aaccc83cd3df3
Michał <michal.dwojak92@gmail.com>
2016-12-02 23:55:44 +0100

    Add - Class and Method insert /delete, select

---
+++ Insert_delete.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login_Application
+{
+    class Insert_delete
+    {
+        public string Komenda;
+
+
+        public void dane_do_edycji()
+        {
+            System.Data.SqlClient.SqlConnection M1conn;
+            M1conn = new SqlConnection();
+            M1conn.ConnectionString = Properties.Settings.Default.agent2214_db1portfolioConnectionString1;
+            M1conn.Open();
+            System.Data.SqlClient.SqlCommand M1command = new SqlCommand();
+            M1command.Connection = M1conn;
+            M1command.CommandText = "" + Komenda + "";
+            M1command.ExecuteNonQuery();
+            M1conn.Close();
+        }
+
+    }
+}
---
+++ Simple_select.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login_Application
+{
+    class Simple_select
+    {
+        SqlConnection con = new SqlConnection(Properties.Settings.Default.agent2214_db1portfolioConnectionString1);
+
+        public DataSet select(string zapytanie, string baza)
+        {
+            DataSet testowe3 = new DataSet();
+            SqlDataAdapter adapter_testowe3 = new SqlDataAdapter(" " + zapytanie + "", con);
+            adapter_testowe3.Fill(testowe3, "" + baza + "");
+            return testowe3;
+        }
+    }
+}
