9c6d0bef589ac26ab3d778ddbe5344a5e98b4926
Michał <michal.dwojak92@gmail.com>
2016-12-04 23:47:11 +0100

    Edit - class - uzytkownik_zalogowany

--- Uzytkownik_zalogowany.cs
+++ Uzytkownik_zalogowany.cs
@@ -8,5 +8,35 @@ namespace Login_Application
 {
     class Uzytkownik_zalogowany
     {
+        private static string _zalogowany;
+        //public static string zalogowany { get; set; }
+        //private static string _czas_zalogowania;
+        //you can choice
+        public static string zalogowany
+        {
+            get
+            {
+                return _zalogowany;
+            }
+
+
+            set
+            {
+                _zalogowany = value;
+            }
+        }
+
+
+
+        public static string czas_zalogowania { get; set; }
+
+        public static string zalogowany_czas_zalogowania()
+        {
+            string x = String.Format("{0}, pracujesz na aplikacji od: {1}", _zalogowany, czas_zalogowania);
+            return x;
+        }
+
+
     }
+}
 }
