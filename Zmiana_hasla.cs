891f51bd913f1d2f1a690953cf78775cd4ff2151
Michał <michal.dwojak92@gmail.com>
2016-12-04 23:45:43 +0100

    Add - class: zmiana hasla, uzytkownik_zalogowany

---
+++ Zmiana_hasla.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login_Application
+{
+    class Zmiana_hasla
+    {
+
+        public static string zmiana_hasla_na_prawdziwe(string x, string x1)
+        {
+            x = x + x1;
+
+            return x;
+        }
+    }
+}
